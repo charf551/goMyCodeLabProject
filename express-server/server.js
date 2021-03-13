57d9d0c3e094a77d729661658b3d5c8cb1d5f40c
charf551 <charfeddine.chebbah@gmail.com>
2021-03-13 16:55:50 +0100

    Update server.js

--- express-server/server.js
+++ express-server/server.js
@@ -38,4 +38,4 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+server.listen(port,'gomycodelabproject_express_1', () => console.log(`API running on localhost:${port}`));
